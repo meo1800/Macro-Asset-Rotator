c21ba9ff7e65a370d5f595aa347abca61e245eb6
ahmed elzoghiby <suktort@gmail.com>
2025-07-27 15:53:41 +0400

    style(backtest.py): added white space to output

--- backtest.py
+++ backtest.py
@@ -21,7 +21,7 @@ assets = pd.concat(data.values(), axis = 1)
 assets.dropna(inplace = True)
 
 # Find strategy returns
-print("Generating strategy...")
+print("\nGenerating strategy...\n")
 strategy_returns = generate_rotation_strategy(assets)
 
 # Find Buy & Hold SPY and equal weighted portfolio returns for comparison
@@ -29,7 +29,7 @@ buy_and_hold_returns = assets["SPY"].pct_change().dropna()
 equal_weighted_returns = assets.pct_change().dropna().mean(axis = 1)
 
 # Evaluate performance by finding cumulative return
-print("Rotation Strategy:\n", evaluate_performance(strategy_returns))
+print("\nRotation Strategy:\n", evaluate_performance(strategy_returns))
 print("\nBuy & Hold:\n", evaluate_performance(buy_and_hold_returns))
 print("\nEqual Weighted:\n", evaluate_performance(equal_weighted_returns))
 
