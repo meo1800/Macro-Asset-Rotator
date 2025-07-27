d7eba641a4231c43cc5bde9cec88c5e318675337
ahmed elzoghiby <suktort@gmail.com>
2025-07-27 15:51:12 +0400

    feat(backtest.py): generated and printed strategy returns and compared to others

--- backtest.py
+++ backtest.py
@@ -18,7 +18,19 @@ for symbol in tickers:
 
 # Concatenates the data for each ticker into one data frame
 assets = pd.concat(data.values(), axis = 1)
-
 assets.dropna(inplace = True)
 
-print(assets.tail())
+# Find strategy returns
+print("Generating strategy...")
+strategy_returns = generate_rotation_strategy(assets)
+
+# Find Buy & Hold SPY and equal weighted portfolio returns for comparison
+buy_and_hold_returns = assets["SPY"].pct_change().dropna()
+equal_weighted_returns = assets.pct_change().dropna().mean(axis = 1)
+
+# Evaluate performance by finding cumulative return
+print("Rotation Strategy:\n", evaluate_performance(strategy_returns))
+print("\nBuy & Hold:\n", evaluate_performance(buy_and_hold_returns))
+print("\nEqual Weighted:\n", evaluate_performance(equal_weighted_returns))
+
+
