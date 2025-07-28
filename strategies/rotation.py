b6f4611f23b29ef555b4777d25ebc515e36ab3a3
ahmed elzoghiby <suktort@gmail.com>
2025-07-28 04:30:44 +0400

    feat(rotation.py): factored slippage into main strategy function

--- strategies/rotation.py
+++ strategies/rotation.py
@@ -1,9 +1,10 @@
 import pandas as pd
+from utils.slippage import apply_slippage
 
 def generate_rotation_strategy(assets, window = 21):
 
     print("\nGenerating strategy...\n")
-    
+
     # Finding rotation single based on return of trailing window
     trailing_returns = assets.pct_change(periods = window)
     rotation_signal = trailing_returns.dropna().idxmax(axis = 1)
@@ -12,11 +13,12 @@ def generate_rotation_strategy(assets, window = 21):
     # Realign signal to rotate based on previous days asset and reindex
     aligned_signal = rotation_signal.shift(1).reindex(daily_returns.index).dropna()
 
-    # Calculating the strategy returns based on aligned signal
+    # Calculating the strategy returns based on aligned signal factoring in slippage
     strategy_returns = []
     for date, asset in aligned_signal.items():
-        value = daily_returns.loc[date, asset]
-        strategy_returns.append(value)
+        raw_return = daily_returns.loc[date, asset] # No slippage (float)
+        slippage_return = apply_slippage(pd.Series([1 + raw_return])).iloc[0] - 1 # Converts float>>list>>series and applies slippage 
+        strategy_returns.append(slippage_return)
 
     # Indexing strategy_returns as a series
     strategy_returns = pd.Series(strategy_returns, index = aligned_signal.index)
