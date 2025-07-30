1f70b236b78a399bd41144d1465dc45e949559aa
ahmed elzoghiby <suktort@gmail.com>
2025-07-31 02:36:48 +0400

    feat(baseline_returns.py): added get_baseline_returns function

--- utils/baseline_returns.py
+++ utils/baseline_returns.py
@@ -1,2 +1,8 @@
 import pandas as pd
 
+def get_base_line_returns(assets):
+    # Find Buy & Hold SPY and equal weighted portfolio returns for comparison
+    buy_and_hold_returns = assets["SPY"].pct_change().dropna()
+    equal_weighted_returns = assets.pct_change().dropna().mean(axis = 1)
+    return buy_and_hold_returns, equal_weighted_returns
+
