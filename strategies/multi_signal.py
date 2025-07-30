73d836dde0002a5562bdef72b03480775f93be59
ahmed elzoghiby <suktort@gmail.com>
2025-07-31 02:17:14 +0400

    feat(multi_signal.py): applied signal to each dely scenario of returns

--- strategies/multi_signal.py
+++ strategies/multi_signal.py
@@ -14,3 +14,23 @@ def generate_multi_signal_strategy(assets, window = 21, volatility_window = 5, m
     momentum_rank = momentum.rank(axis = 1, ascending = False)
     volatility_rank = volatility.rank(axis = 1, ascending = True)
     combined_score = momentum_rank + volatility_rank
+
+    # Signal returns asset column with lowest score (best rank)
+    signal = combined_score.dropna().idxmin(axis = 1)
+
+    daily_returns = assets.pct_change().dropna()
+    
+    results = {}
+
+    # Appyling signal to each delay scenario in the range
+    for delay in range (1, max_delay + 1):
+        shifted_signal = signal.shift(delay).reindex(daily_returns.index).dropna()
+        returns = []
+
+        for date, asset in shifted_signal.items():
+            returns.append(daily_returns.loc[date, asset])
+        strategy_returns = pd.Series(returns, index = shifted_signal.index)
+        results[delay] = strategy_returns
+
+    return results
+
